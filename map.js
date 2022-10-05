398c3ac3e165ad6b70b1a37820661846572146ad
dev1ninja <devninja.up@gmail.com>
2022-10-04 20:32:53 -0500

    add custom geocoding service url

--- map.js
+++ map.js
@@ -9,7 +9,7 @@ var routeResult, hasResult = false;
 var routeControl = L.Routing.control({
     waypoints: startLocation,
     serviceUrl: 'https://osrm.mgoconnect.org/route/v1',
-    geocoder: L.Control.Geocoder.nominatim({ serviceUrl: "Geo.mygovernmentonline.org" }),
+    geocoder: L.Control.Geocoder.nominatim({ serviceUrl: "http://geo.mygovernmentonline.org/" }),
     routeWhileDragging: true,
     show: true,
 }).on('routeselected', function(e) {
@@ -22,7 +22,7 @@ var address;
 
 // 2022.9.30 update
 
-var geocoder = L.Control.Geocoder.nominatim({ serviceUrl: "Geo.mygovernmentonline.org" });
+var geocoder = L.Control.Geocoder.nominatim({ serviceUrl: "http://geo.mygovernmentonline.org/" });
 
 function codeAddress() {
     let arr = [];
